0a335d6ce48ff4fcc1d8e051c13b51e71c61b4f3
Unknown <zo.ravalitera@outlook.com>
2018-06-07 15:48:55 +0300

    Page pourquoi

--- wp-content/themes/ivana/page-concept.php
+++ wp-content/themes/ivana/page-concept.php
@@ -1,3 +1,48 @@
 <?php
-/*Template name: Concept page*/
+/*Template name: Full page layout*/
 get_header();?>
+
+<main class="main_single" id="fullWidth-page">
+    <section class="fullSection grey centered">
+        <div class="wrapper">
+            <div class="head-page clearfix">
+                <h1>Notre pourquoi</h1>
+                <img src="<?php echo get_template_directory_uri();?>/images/notre_pourquoi.jpg" >
+                <img src="<?php echo get_template_directory_uri();?>/images/notre_pourquoi2.jpg" >
+            </div>
+            <h2>Madagascar, nous y croyons.</h2>
+            <p>On vous a peut être déjà dit « Madagascar, c’est pas évident ». Peut être même que quelqu’un vous a déjà découragé d’y aller.</p>
+            <p>C’est pire quand vos amis vous disent qu’ils ont construit leur villa « Any Dago », mais que ça ne s’est pas bien passé : problème de papiers, de suivi des travaux, de main d’oeuvre, bref, un vrai parcours du combattant.</p>
+            <p>Et pourtant vous avez toujours envie d’y croire. Parce que Madagascar, c’est quand même chez vous.</p>
+            <p>Si vous vous retrouvez la dedans, vous êtes au bon endroit.</p>
+            <p>En fait, ce n’est pas une question d’argent, ni une histoire de contexte économique, ni même de formalités administratives compliquées.</p>
+
+            <h2>C’est juste que notre pays nous prend aux tripes !</h2>
+            <p><strong>Nous avons la certitude qu’à partir du moment où vous vous êtes décidé à</strong>, investir
+            à Madagascar, vous y installer un temps ou pour votre retraite, nous confier la
+            construction de votre maison, c'est un pas de plus vers un renouveau de l’Ile !</p>
+
+            <h2>On aime ce que vous aimez</h2>
+            <p>Vous êtes partis du pays il y a peut-être 10, 15, ou 20 ans.
+            Lorsque vous rentrez, il y a des choses que vous ne comprenez pas. Peut être que
+            vous vous demandez pourquoi les gens achètent des matériaux de mauvaise qualité
+            ? Pourquoi les finitions sont mal faites ?
+            C’est juste une question d’état d’esprit.
+            Nous partons du principe que le travail bien fait avec de bons matériaux est une
+            évidence a Madagascar, si on y met tout son coeur.</p>
+
+            <div class="the_founder">
+                <strong class="name">Lilia Ratefiarivony</strong>
+                <span class="post">Fondatrice et Manager</span>
+            </div>
+        </div>
+    </section>
+    <section class="fullSection bloc-rdv centered">
+        <div class="wrapper">
+            <h2>Et si on en parlait autour d’un cafe?</h2>
+            <a href="javascript:void(0)">Se voir  à PARIS</a>
+            <a href="javascript:void(0)"n>Se voir à Tana</a>
+        </div>
+    </section>
+        
+<?php get_footer();?>
