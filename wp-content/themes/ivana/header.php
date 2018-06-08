3e879516565e17bbece3af61f12922209af23828
Unknown <volatiana.helisoa@yahoo.fr>
2018-06-08 12:06:31 +0300

    map terrain

--- wp-content/themes/ivana/header.php
+++ wp-content/themes/ivana/header.php
@@ -2,97 +2,83 @@
 
 <html>
 
-<head>
+    <head>
 
-	<title><?php wp_title( '|', true, 'right' ); ?>IVANA</title>
+        <title><?php wp_title( '|', true, 'right' ); ?>IVANA</title>
 
-	<meta http-equiv="content-type" content="text/html" />
+        <meta http-equiv="content-type" content="text/html" />
 
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-	<meta name="author" content="lolkittens" />
+        <meta name="author" content="lolkittens" />
 
-    <link rel="icon" href="<?php bloginfo( 'template_url' ); ?>/images/favicon.ico"/>
+        <link rel="icon" href="<?php bloginfo( 'template_url' ); ?>/images/favicon.ico"/>
 
-    <link href="<?php bloginfo( 'template_url' ); ?>/style.css" rel="stylesheet"/>
-    
-    <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/bootstrap.css" rel="stylesheet"/>
+        <link href="<?php bloginfo( 'template_url' ); ?>/style.css" rel="stylesheet"/>
 
-    <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/star-rating.css" rel="stylesheet"/>
+        <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/bootstrap.css" rel="stylesheet"/>
 
-    <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/bootstrap-datetimepicker.min.css" rel="stylesheet"/>
+        <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/star-rating.css" rel="stylesheet"/>
 
-    <link href="<?php bloginfo( 'template_url' ); ?>/css/mobile.css" rel="stylesheet"/>
+        <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/bootstrap-datetimepicker.min.css" rel="stylesheet"/>
 
-    <link href="<?php bloginfo( 'template_url' ); ?>/css/pageStyle.css" rel="stylesheet"/>
+        <link href="<?php bloginfo( 'template_url' ); ?>/css/mobile.css" rel="stylesheet"/>
 
-    <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/slick.css" rel="stylesheet"/>
+        <link href="<?php bloginfo( 'template_url' ); ?>/css/pageStyle.css" rel="stylesheet"/>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/jquery.js"></script>
+        <link href="<?php bloginfo( 'template_url' ); ?>/css/lib/slick.css" rel="stylesheet"/>
+       
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/slick.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/jquery.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/slick.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/scrollreveal.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/bootstrap.min.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/star-rating.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/scrollreveal.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/moment.min.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/star-rating.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/fr.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/moment.min.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/bootstrap-datetimepicker.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/fr.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/scrollreveal.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/bootstrap-datetimepicker.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/custom.js"></script>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/lib/scrollreveal.js"></script>
 
-    <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/rating.js"></script>
-  
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/custom.js"></script>
 
-</head>
+        <script type="text/javascript" src="<?php bloginfo( 'template_url' ); ?>/js/rating.js"></script>
 
-<?php  wp_head();?>
 
-<body class="">
+    </head>
 
-    <input type="hidden" id="valuPlan" />
+    <?php  wp_head();?>
+
+    <body class="">
+
+        <input type="hidden" id="valuPlan" />
 
     <header>
 
-       <div class="container relative">
-
+        <div class="container relative">
             <div class="logo text-center">
-
                 <a href="<?php echo get_settings('home'); ?>">
-
                     <img src="<?php bloginfo( 'template_url' ); ?>/images/logo.png" class="img-responsive"  />
-
                 </a>
-
             </div>
 
             <div class="menuHeader">
-
                 <div class="navbar-header ">
-
                     <button type="button" class="navbar-toggle">
-
                         <span class="icon-bar"></span>
-
                     </button>    
-
                 </div>
 
                 <div class="mehuHeader">
-
                     <nav class="navMenu">
-
                         <?php wp_nav_menu(array('theme_location' => 'primary', 'container' => '', 'items_wrap' => '<ul class="pull-left nav navbar-nav main-nav">%3$s</ul>',)); ?>
-
-                        
-
                     </nav>
 
                 </div>
